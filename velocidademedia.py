e3eb04c0efdcb12d7417c8e8386bb2107a2ad0b6
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-03-27 16:10:49 -0300

    Terceiro Commit

--- velocidademedia.py
+++ velocidademedia.py
@@ -3,6 +3,13 @@ def __init__():
     tempo = int(input("Insira o tempo gasto (em h): "))
     velocidadeMedia = distancia / tempo
     print("A sua velocidade média nesse percurso foi: ", velocidadeMedia, " km/h")
-
+    rodarNovamente()
+    
+def rodarNovamente():
+    escolha = input("Deseja calcular outra velocidade média?\nSim: Aperte um.\nNão: Aperta qualquer outra coisa.\n")
+    if (escolha == 1):
+        __init__()
+    else:
+        exit
     
 __init__()
